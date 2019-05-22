6e0a5268853a2824600bb6e39d7df4637292d7d5
hammerofmathematics <49748478+hammerofmathematics@users.noreply.github.com>
2019-05-22 19:49:05 +0800

    Add files via upload

--- h3.py
+++ h3.py
@@ -1,2 +1,21 @@
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+g = fib(5)
+while 1:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+f = fib(10)
+print('fib(10):', f)
+for x in f:
+    print(x)
 
 
